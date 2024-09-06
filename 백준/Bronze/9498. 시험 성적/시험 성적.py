2ac6c9bfddb06e9ffc05e027d45a099fa721ef5f
DoungHyunKim <157472220+DoungHyunKim@users.noreply.github.com>
2024-09-06 16:26:25 +0900

    [Bronze V] Title: 시험 성적, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+# 두 수 비교하기 
+res = int(input())
+if 90<=res<=100:
+    print("A")
+elif 80<=res<=89:
+    print("B")
+elif 70<=res<=79:
+    print("C")
+elif 60<=res<=69:
+    print("D")
+else:
+    print("F")
