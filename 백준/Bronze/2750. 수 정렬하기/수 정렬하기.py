6530e64d36fa387bfae6e1e641dc5079bf6899de
DoungHyunKim <157472220+DoungHyunKim@users.noreply.github.com>
2024-09-09 08:24:29 +0900

    [Bronze II] Title: 수 정렬하기, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -0,0 +1,13 @@
+N = int(input())
+num_list = []
+
+# N번 입력 받기
+for _ in range(N):
+    num_list.append(int(input()))
+
+# 중복된 수 제거 및 정렬
+num_list = sorted(set(num_list))
+
+# 오름차순 정렬된 리스트 출력
+for num in num_list:
+    print(num)
