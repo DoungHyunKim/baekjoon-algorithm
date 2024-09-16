64bc8a5507ddb5bf8a594a1a1b2c64c85fac27cc
DoungHyunKim <157472220+DoungHyunKim@users.noreply.github.com>
2024-09-16 14:41:26 +0900

    [Silver IV] Title: 카드2, Time: 224 ms, Memory: 55036 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -0,0 +1,13 @@
+from collections import deque
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+N = int(input())
+
+arr = deque([i for i in range(1,N+1)])
+
+while len(arr) > 1:
+    arr.popleft()
+    arr.append(arr.popleft())
+
+print(arr[0]) 
