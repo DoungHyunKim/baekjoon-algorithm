36726cfd7fb9b70a4730d7e66c8008f3158bb2bd
DoungHyunKim <157472220+DoungHyunKim@users.noreply.github.com>
2024-09-27 20:16:28 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -1,16 +1,10 @@
-def fibo(N):
-    if N == 0:
-        return 0
-    elif N == 1:
-        return 1
-    
-    if dp[N] != 0:
-        return dp[N]
-    
-    dp[N] = fibo(N-1) + fibo(N-2)
-
-    return dp[N]
-
 N = int(input())
+
 dp = [0] * (N+1)
-print(fibo(N))
+dp[0] = 0
+dp[1] = 1
+
+for i in range(2, N+1):
+    dp[i] = dp[i-1] + dp[i-2]
+
+print(dp[N])
