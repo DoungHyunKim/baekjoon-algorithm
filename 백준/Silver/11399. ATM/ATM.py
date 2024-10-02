b4a61f26957d970a5df1a05d51c09d5a210f1abd
DoungHyunKim <157472220+DoungHyunKim@users.noreply.github.com>
2024-10-03 08:45:35 +0900

    [Silver IV] Title: ATM, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -2,22 +2,12 @@ import sys
 
 input = lambda: sys.stdin.readline().rstrip()
 
-N = int(input())
+n = int(input())  
+peoples = list(map(int, input().split()))  
 
-times = list(map(int, input().split()))
-times = sorted(times)
+peoples.sort()  
+answer = 0  
 
-per_time = []
-
-for i in range(len(times)+1):
-    per_time.append(times[:i])
-
-# print(per_time)
-time_sum= 0
-
-for i in per_time:
-    for j in i:
-        time_sum += j
-        # print(time_sum)
-
-print(time_sum)
+for i in range(1, n+1):
+    answer += sum(peoples[:i])  
+print(answer) 
